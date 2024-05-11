b41190f9aa62ae7525e66210f699be6d26b4e4ab
Cyberfire <buayagoreng99@gmail.com>
2024-05-11 12:32:42 +0800

    membuat kelas node linked list menggunakan akses public

--- Stackusinglinkedlist_0154/Stackusinglinkedlist_0154.cpp
+++ Stackusinglinkedlist_0154/Stackusinglinkedlist_0154.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+using namespace std;
+
+// Kelas Node yang merepresentasikan satu node dalam linked list
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
 int main()
 {
 }
